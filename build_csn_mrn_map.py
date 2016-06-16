a453e16f6a9313791bcea7b733efd3f108e6536e
yoni <halpern@cs.nyu.edu>
2016-06-16 14:33:31 -0400

    added workingdir arg to build_csn_mrn_map

--- build_csn_mrn_map.py
+++ build_csn_mrn_map.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 import cPickle as pickle
+import sys
+
+working_dir = sys.argv[1]
 
 infile = file('data/SEPSIS_COHORT_2013-2014.DEID.csv')
 
@@ -12,10 +15,9 @@ for l in infile:
     csn_to_mrn[csn] = mrn
 
 print len(mrn_to_csn), 'mrn values'
-pickle.dump(mrn_to_csn, file('patients/mrn_to_csn.pk', 'w'))
-pickle.dump(csn_to_mrn, file('patients/csn_to_mrn.pk', 'w'))
-
-outfile = file('patients/all_csn', 'w')
+pickle.dump(mrn_to_csn, file(working_dir+'/patients/mrn_to_csn.pk', 'w'))
+pickle.dump(csn_to_mrn, file(working_dir+'/patients/csn_to_mrn.pk', 'w'))
+outfile = file(working_dir+'/patients/all_csn', 'w')
 for csn in csn_to_mrn:
   print >>outfile, csn
 outfile.close()
